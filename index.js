92acb03f0aab18fb5e8e32916d44dfea96aa9159
Antoine Arlaud <aarlaud@hotmail.com>
2018-10-04 13:52:17 -0400

    dsfsd

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-const jq = require('node-jq');
 const jsonPath = './sample/movies.json';
 
 const filter = '.movies[].title'
@@ -8,11 +7,3 @@ const options = {}
 
 console.log("Star Wars titles");
 
-jq.run(filter, jsonPath, options)
-  .then((output) => {
-    console.log(output)
-  })
-  .catch((err) => {
-    console.error(err)
-    // Something went wrong...
-  })
